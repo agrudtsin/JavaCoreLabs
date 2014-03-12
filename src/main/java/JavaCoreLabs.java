74a0124358bc1dd1966f8bf8e3c3e346074023c5
unknown <@andreysNotebook.(none)>
2014-03-13 00:19:41 +0200

    Initial commit

---
+++ src/main/java/JavaCoreLabs.java
@@ -0,0 +1,14 @@
+import java.text.DateFormatSymbols;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Андрей on 10.03.14.
+ */
+public class JavaCoreLabs {
+    public static void main(String args[]){
+
+    }
+}
